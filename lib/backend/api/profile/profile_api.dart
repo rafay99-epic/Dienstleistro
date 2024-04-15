a3321f52571b0ef4b0e61991f1c8b206fc03b218
Abdul Rafay <99marafay@gmail.com>
2024-04-15 19:29:56 +0500

    Profile Update API: Code Optimization

--- lib/backend/api/profile/profile_api.dart
+++ lib/backend/api/profile/profile_api.dart
@@ -3,6 +3,33 @@ import 'dart:io';
 import 'dart:convert';
 
 class ProfileAPI {
+  //Request Methods
+  Future<bool> postRequest(String url, Map<String, String> body) async {
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> responseBody = jsonDecode(response.body);
+        if (responseBody['success']) {
+          print('Request successful');
+          return true;
+        } else {
+          print('Failed to make request: ${responseBody['message']}');
+          return false;
+        }
+      } else {
+        print('Failed to make request. Status code: ${response.statusCode}');
+        return false;
+      }
+    } catch (e) {
+      print('An error occurred: $e');
+      return false;
+    }
+  }
+
   //basic profile
   Future<bool> basicUpdate({
     required File bannerImage,
@@ -52,7 +79,6 @@ class ProfileAPI {
       var response = await request.send();
 
       if (response.statusCode == 200) {
-        // If the server returns a 200 OK response, then parse the JSON.
         final respStr = await response.stream.bytesToString();
         Map<String, dynamic> responseBody = jsonDecode(respStr);
         if (responseBody['success']) {
@@ -63,7 +89,6 @@ class ProfileAPI {
           return false;
         }
       } else {
-        // If the server returns a response code other than 200, throw an exception.
         print('Failed to update profile. Status code: ${response.statusCode}');
         return false;
       }
@@ -73,87 +98,46 @@ class ProfileAPI {
     }
   }
 
-  // Update Education:
+  //update Education
   Future<bool> updateEducation({
     required String title,
     required String type,
     required String startYear,
     required String endYear,
     required String summary,
-  }) async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://dienstleisto.de/api/education'),
-        body: {
-          'd_title[0]': title,
-          'd_type[0]': type,
-          'd_syear[0]': startYear,
-          'd_eyear[0]': endYear,
-          'd_summary[0]': summary,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        // If the server returns a 200 OK response, then parse the JSON.
-        Map<String, dynamic> responseBody = jsonDecode(response.body);
-        if (responseBody['success']) {
-          print('Education updated successfully');
-          return true;
-        } else {
-          print('Failed to update education: ${responseBody['message']}');
-          return false;
-        }
-      } else {
-        // If the server returns a response code other than 200, throw an exception.
-        print(
-            'Failed to update education. Status code: ${response.statusCode}');
-        return false;
-      }
-    } catch (e) {
-      print('An error occurred: $e');
-      return false;
-    }
+  }) {
+    return postRequest('http://dienstleisto.de/api/education', {
+      'd_title[0]': title,
+      'd_type[0]': type,
+      'd_syear[0]': startYear,
+      'd_eyear[0]': endYear,
+      'd_summary[0]': summary,
+    });
   }
 
-  // Update Experience:
+  //update Expereince
   Future<bool> updateExperience({
     required String title,
     required String company,
     required String companyStartDate,
     required String summary,
     required String companyEndDate,
-  }) async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://dienstleisto.de/api/experience'),
-        body: {
-          'etitle[0]': title,
-          'ecomany[0]': company,
-          'companysd[0]': companyStartDate,
-          'esummry[0]': summary,
-          'companyed[0]': companyEndDate,
-        },
-      );
+  }) {
+    return postRequest('http://dienstleisto.de/api/experience', {
+      'etitle[0]': title,
+      'ecomany[0]': company,
+      'companysd[0]': companyStartDate,
+      'esummry[0]': summary,
+      'companyed[0]': companyEndDate,
+    });
+  }
 
-      if (response.statusCode == 200) {
-        // If the server returns a 200 OK response, then parse the JSON.
-        Map<String, dynamic> responseBody = jsonDecode(response.body);
-        if (responseBody['success']) {
-          print('Experience updated successfully');
-          return true;
-        } else {
-          print('Failed to update experience: ${responseBody['message']}');
-          return false;
-        }
-      } else {
-        // If the server returns a response code other than 200, throw an exception.
-        print(
-            'Failed to update experience. Status code: ${response.statusCode}');
-        return false;
-      }
-    } catch (e) {
-      print('An error occurred: $e');
-      return false;
-    }
+  // Update Skills
+  Future<bool> updateSkills({
+    required List<String> skills,
+  }) {
+    return postRequest('http://dienstleisto.de/api/skills', {
+      'skills': skills.join(','),
+    });
   }
 }
